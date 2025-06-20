7473dec259437617169c9158dceb9ca6ab9152f7
JDBG99 <josedavidblanco720@gmail.com>
2025-06-20 10:30:04 +0200

    Clase estática
    Clase estática

---
+++ Mensajes.java
@@ -0,0 +1,5 @@
+public class Mensajes {
+    public static final String ERROR_NOTA1 = "La nota1 debe estar entre 0 y 10.";
+    public static final String ERROR_NOTA2 = "La nota2 debe estar entre 0 y 10.";
+    public static final String ERROR_NOTA3 = "La nota3 debe estar entre 0 y 10.";
+}
